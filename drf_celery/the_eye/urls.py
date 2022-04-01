817b0122b7a3e2521031875e290cd4aadb5a4d91
Jose Renato <jrenato-mba@jrenato-mba.Dlink>
2022-04-01 10:43:55 -0300

    Added app urls to the main urls list

---
+++ drf_celery/the_eye/urls.py
@@ -0,0 +1,12 @@
+"""
+This module create
+"""
+from django.urls import path
+
+from .views import store_event
+
+app_name = 'the_eye'
+
+urlpatterns = [
+    path('/', store_event, name='event_save'),
+]
