71cad1e4ab795c4612ad5f6cfcd260d2d079d555
Jose Renato <jrenato-mba@jrenato-mba.Dlink>
2022-04-01 10:25:43 -0300

    Added serializers

---
+++ drf_celery/the_eye/serializers.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+from rest_framework import serializers
+
+
+class EventSerializer(serializers.Serializer):
+    session_id = serializers.UUIDField()
+    category = serializers.CharField()
+    name = serializers.CharField()
+    timestamp = serializers.DateTimeField()
+    data = serializers.DictField()
+
+    def validate_timestamp(self, attrs, source):
+        """
+        Check if the timestamp is in the future
+        """
+        value = attrs[source]
+        if value > datetime.now():
+            raise serializers.ValidationError("The timestamp is in the future!")
+        return attrs
+
+
+class PageViewSerializer(serializers.Serializer):
+    host = serializers.CharField()
+    path = serializers.CharField()
+
+
+class PageClickSerializer(serializers.Serializer):
+    host = serializers.CharField()
+    path = serializers.CharField()
+    element = serializers.CharField()
+
+
+class AccountFieldsSerializer(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+
+
+class AccountSubmitFormSerializer(serializers.Serializer):
+    host = serializers.CharField()
+    path = serializers.CharField()
+    form = AccountFieldsSerializer()
