20afc9ba51b1d245d918d36ce5f9573f84bf917c
Jose Renato <jrenato-mba@jrenato-mba.Dlink>
2022-04-01 10:43:20 -0300

    Create initial view

--- drf_celery/the_eye/views.py
+++ drf_celery/the_eye/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['POST'])
+def store_event(request):
+    """
+    Receive the event data and pushes to the queue to be processed later.
+    """
+    import pprint
+    pprint.pprint(request.data)
+
+    return Response({"success": True})
