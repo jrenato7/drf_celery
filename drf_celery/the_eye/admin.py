822dec47e12f1403e3b16860b343e66c7ce31d27
Jose Renato <jrenato-mba@jrenato-mba.Dlink>
2022-04-03 20:58:38 -0300

    Update event admin list fields

--- drf_celery/the_eye/admin.py
+++ drf_celery/the_eye/admin.py
@@ -7,5 +7,9 @@ class ErrorLogModelAdmin(admin.ModelAdmin):
     list_display = ['created_at', 'message']
 
 
+class EventModelAdmin(admin.ModelAdmin):
+    list_display = ['session_id', 'category', 'name', 'timestamp']
+
+
 admin.site.register(ErrorLog, ErrorLogModelAdmin)
-admin.site.register(Event)
+admin.site.register(Event, EventModelAdmin)
