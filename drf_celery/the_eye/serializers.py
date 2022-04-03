a4c3e1db4314baac42b9b9f2def126b78aa9e59d
Jose Renato <jrenato-mba@jrenato-mba.Dlink>
2022-04-03 17:29:31 -0300

    Validating payload: avoid unknown fields

--- drf_celery/the_eye/serializers.py
+++ drf_celery/the_eye/serializers.py
@@ -5,6 +5,16 @@ from rest_framework import serializers
 from .models import Event, PageView, PageClick, Account, EventForm
 
 
+class ValidatePayload:
+    def has_unknowns(self, attrs):
+        """Verify if the data informed has unknown fields"""
+
+        unknown = set(self.initial_data) - set(self.fields)
+        if unknown:
+            raise serializers.ValidationError("Unknown field(s): {}".format(", ".join(unknown)))
+        return attrs
+
+
 class EventSerializer(serializers.ModelSerializer):
     timestamp = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S.%f")
     data = serializers.DictField()
@@ -22,30 +32,46 @@ class EventSerializer(serializers.ModelSerializer):
         return value
 
 
-class PageViewSerializer(serializers.ModelSerializer):
+class PageViewSerializer(serializers.ModelSerializer, ValidatePayload):
     class Meta:
         model = PageView
         exclude = ('event', )
 
+    def validate(self, attrs):
+        attrs = super().has_unknowns(attrs)
+        return super().validate(attrs)
 
-class PageClickSerializer(serializers.Serializer):
+
+class PageClickSerializer(serializers.Serializer, ValidatePayload):
     class Meta:
         model = PageClick
         exclude = ('event', )
 
+    def validate(self, attrs):
+        attrs = super().has_unknowns(attrs)
+        return super().validate(attrs)
 
-class AccountSerializer(serializers.Serializer):
+
+class AccountSerializer(serializers.Serializer, ValidatePayload):
     class Meta:
         model = Account
         exclude = ('event_form', )
 
+    def validate(self, attrs):
+        attrs = super().has_unknowns(attrs)
+        return super().validate(attrs)
 
-class EventFormSerializer(serializers.Serializer):
+
+class EventFormSerializer(serializers.Serializer, ValidatePayload):
     form = AccountSerializer()
 
     class Meta:
         model = EventForm
         exclude = ('event', )
+
+    def validate(self, attrs):
+        attrs = super().has_unknowns(attrs)
+        return super().validate(attrs)
 
     def create(self, validated_data):
         account_data = validated_data.pop('form', None)
