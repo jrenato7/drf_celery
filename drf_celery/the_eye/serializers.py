ba4fe1f6cd4e64c08ef80bac815df6e734aee5b3
Jose Renato <jrenato-mba@jrenato-mba.Dlink>
2022-04-03 16:53:56 -0300

    Update serializer to use models definition

--- drf_celery/the_eye/serializers.py
+++ drf_celery/the_eye/serializers.py
@@ -1,40 +1,61 @@
+from django.utils import timezone
+
 from rest_framework import serializers
 
+from .models import Event, PageView, PageClick, Account, EventForm
 
-class EventSerializer(serializers.Serializer):
-    session_id = serializers.UUIDField()
-    category = serializers.CharField()
-    name = serializers.CharField()
+
+class EventSerializer(serializers.ModelSerializer):
     timestamp = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S.%f")
     data = serializers.DictField()
 
-    # def validate_timestamp(self, attrs, source):
-    #     """
-    #     Check if the timestamp is in the future
-    #     """
-    #     value = attrs[source]
-    #     if value > datetime.now():
-    #         raise serializers.ValidationError("The timestamp is in the future!")
-    #     return attrs
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+    def validate_timestamp(self, value):
+        """
+        Check if the timestamp is in the future
+        """
+        if value > timezone.now():
+            raise serializers.ValidationError("The timestamp is in the future!")
+        return value
 
 
-class PageViewSerializer(serializers.Serializer):
-    host = serializers.CharField()
-    path = serializers.CharField()
+class PageViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PageView
+        exclude = ('event', )
 
 
 class PageClickSerializer(serializers.Serializer):
-    host = serializers.CharField()
-    path = serializers.CharField()
-    element = serializers.CharField()
+    class Meta:
+        model = PageClick
+        exclude = ('event', )
 
 
-class AccountFieldsSerializer(serializers.Serializer):
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
+class AccountSerializer(serializers.Serializer):
+    class Meta:
+        model = Account
+        exclude = ('event_form', )
 
 
-class AccountSubmitFormSerializer(serializers.Serializer):
-    host = serializers.CharField()
-    path = serializers.CharField()
-    form = AccountFieldsSerializer()
+class EventFormSerializer(serializers.Serializer):
+    form = AccountSerializer()
+
+    class Meta:
+        model = EventForm
+        exclude = ('event', )
+
+    def create(self, validated_data):
+        account_data = validated_data.pop('form', None)
+
+        event_form = EventForm.objects.create(**validated_data)
+        event_form.save()
+
+        account_data["event_form_id"] = event_form.id
+
+        account = Account.objects.create(**account_data)
+        account.save()
+
+        return event_form
