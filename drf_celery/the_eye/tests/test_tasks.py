a0d5fc53d3371940584599e2c20c7bb5dddc6c1b
Jose Renato <jrenato-mba@jrenato-mba.Dlink>
2022-04-03 23:59:28 -0300

    Add unit test for Event Page CLICK type

--- drf_celery/the_eye/tests/test_tasks.py
+++ drf_celery/the_eye/tests/test_tasks.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from the_eye.tasks import validate_event, SerializerError
-from the_eye.models import Event, PageView
+from the_eye.models import Event, PageView, PageClick
 
 
 class ValidateEventPageViewSuccessTest(TestCase):
@@ -12,7 +12,7 @@ class ValidateEventPageViewSuccessTest(TestCase):
             "category": "page interaction",
             "name": "pageview",
             "data": {
-                "host": "www.consumeraffairs.com",
+                "host": "www.cwtech.dev",
                 "path": "/"
             },
             "timestamp": "2022-04-01 00:00:0.000000"
@@ -40,3 +40,48 @@ class ValidateEventPageViewErrorTest(TestCase):
     def test_invalid_payload_page_view(self):
         with self.assertRaises(SerializerError):
             validate_event(self.page_view)
+
+
+class ValidateEventPageClickSuccessTest(TestCase):
+
+    def setUp(self):
+        page_view = {
+            "session_id": "d66ac88b-f801-4984-953f-5c6b266494a0",
+            "category": "page interaction",
+            "name": "link click",
+            "data": {
+                "host": "www.cwtech.dev",
+                "path": "/",
+                "element": "register"
+            },
+            "timestamp": "2022-04-01 00:00:0.000000"
+        }
+        validate_event(page_view)
+
+    def test_create_event(self):
+        self.assertTrue(Event.objects.exists())
+
+    def test_create_page_view(self):
+        self.assertTrue(PageClick.objects.exists())
+
+
+class ValidateEventPageClickErrorTest(TestCase):
+
+    def setUp(self):
+        self.page_view = {
+            "session_id": "d66ac88b-f801-4984-953f-5c6b266494a0",
+            "category": "page interaction",
+            "name": "link click",
+            "data": {
+                "host": "www.cwtech.dev",
+                "path": "/",
+                "element": "register",
+                "foo": "bar"
+            },
+            "timestamp": "2022-04-01 00:00:0.000000"
+        }
+
+    def test_invalid_payload_page_click(self):
+        with self.assertRaises(SerializerError):
+            validate_event(self.page_view)
+            self.assertRaisesMessage(SerializerError, 'Payload Invalid!')
