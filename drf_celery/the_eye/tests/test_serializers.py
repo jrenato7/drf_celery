a9279b9463ff7888147546fe6f9ba2f25d599fd1
Jose Renato <jrenato-mba@jrenato-mba.Dlink>
2022-04-04 00:42:23 -0300

    Add test for Validate Payload on EventFormSerializer class

--- drf_celery/the_eye/tests/test_serializers.py
+++ drf_celery/the_eye/tests/test_serializers.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from the_eye.serializers import PageViewSerializer
+from the_eye.serializers import PageViewSerializer, EventFormSerializer
 
 
 class ValidatePayloadSuccessTest(TestCase):
@@ -23,4 +23,32 @@ class ValidatePayloadSuccessTest(TestCase):
     def test_unknown_message(self):
         _assert = self.payload_serializer.errors['non_field_errors'][0]
         expected = "Unknown field(s): invalid_field"
+        self.assertEqual(expected, _assert)
+
+
+class ValidatePayloadEventFormSerializerSuccessTest(TestCase):
+
+    def setUp(self):
+        page_view_paylod = {
+            "host": "www.cwtech.dev",
+            "path": "/",
+            "form": {
+                "first_name": "John",
+                "last_name": "Doe",
+                "password": "invalid value",
+                "other_name": "invalid value"
+            }
+        }
+        self.payload_serializer = EventFormSerializer(data=page_view_paylod)
+        self.payload_serializer.is_valid()
+
+    def test_has_errors(self):
+        self.assertTrue(self.payload_serializer.errors)
+
+    def test_has_unknown_error(self):
+        self.assertIn("non_field_errors", self.payload_serializer.errors)
+
+    def test_unknown_message(self):
+        _assert = self.payload_serializer.errors['non_field_errors'][0]
+        expected = "Unknown field(s): other_name, password"
         self.assertEqual(expected, _assert)
